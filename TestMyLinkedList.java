97720377786fe6574ee2fd3e0b0820646c2e4e80
Administrator <dol.mouse92@gmail.com>
2018-04-20 15:42:13 +0700

    Luyện tập cài đặt cấu trúc LinkedList đơn giản.

---
+++ TestMyLinkedList.java
@@ -0,0 +1,13 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.printf("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+
+        myLinkedList.add(4, 9);
+        myLinkedList.add(4, 9);
+        myLinkedList.printList();
+    }
+}
